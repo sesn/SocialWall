294899c66a850479ad2e0d763d427b6f0a4c2475
sesn <sesankar11@gmail.com>
2018-03-09 21:16:29 +0530

    Initial Server Setup completed

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//creating express app
+const app = express();
+const routes = require('./routes/router');
+
+//Turning raw requests into usable properties on req.body
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use('/', routes);
+
+app.set('port', process.env.PORT || 3887);
+
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
